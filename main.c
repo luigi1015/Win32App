28f173ceb6631cb7f7d7d8d84b4fcf81207d0e0b
luigi1015 <github@314ghz.com>
2025-03-23 19:28:14 -0500

    Converted some spaces to tabs.

--- main.c
+++ main.c
@@ -8,59 +8,59 @@ LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 char szClassName[ ] = "Jeff's App";
 
 int WINAPI WinMain (HINSTANCE hThisInstance,
-                    HINSTANCE hPrevInstance,
-                    LPSTR lpszArgument,
-                    int nFunsterStil)
+					HINSTANCE hPrevInstance,
+					LPSTR lpszArgument,
+					int nFunsterStil)
 
 {
-    HWND hwnd;               /* This is the handle for our window */
-    MSG messages;            /* Here messages to the application are saved */
-    WNDCLASSEX wincl;        /* Data structure for the windowclass */
+	HWND hwnd;			   /* This is the handle for our window */
+	MSG messages;			/* Here messages to the application are saved */
+	WNDCLASSEX wincl;		/* Data structure for the windowclass */
 
-    /* The Window structure */
-    wincl.hInstance = hThisInstance;
-    wincl.lpszClassName = szClassName;
-    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
-    wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
-    wincl.cbSize = sizeof (WNDCLASSEX);
+	/* The Window structure */
+	wincl.hInstance = hThisInstance;
+	wincl.lpszClassName = szClassName;
+	wincl.lpfnWndProc = WindowProcedure;	  /* This function is called by windows */
+	wincl.style = CS_DBLCLKS;				 /* Catch double-clicks */
+	wincl.cbSize = sizeof (WNDCLASSEX);
 
-    /* Use my icon and default mouse-pointer */
-    wincl.hIcon = LoadIcon (hThisInstance, MAKEINTRESOURCE(IDI_MYICON));
-    wincl.hIconSm = LoadIcon (hThisInstance, MAKEINTRESOURCE(IDI_MYICON));
-    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
-    wincl.lpszMenuName = "Menu";
-    // wincl.lpszMenuName = NULL;                 /* No menu */
-    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
-    wincl.cbWndExtra = 0;                      /* structure or the window instance */
-    /* Use Windows's default color as the background of the window */
-    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
-    
-    // Create the menu
-    HMENU hMenu = CreateMenu();
-    HMENU hMenuPopup = CreateMenu();
-    AppendMenu(hMenuPopup, MF_STRING, IDM_FILE_EXIT, TEXT("Exit"));
-    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hMenuPopup, TEXT("File"));
+	/* Use my icon and default mouse-pointer */
+	wincl.hIcon = LoadIcon (hThisInstance, MAKEINTRESOURCE(IDI_MYICON));
+	wincl.hIconSm = LoadIcon (hThisInstance, MAKEINTRESOURCE(IDI_MYICON));
+	wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+	wincl.lpszMenuName = "Menu";
+	// wincl.lpszMenuName = NULL;				 /* No menu */
+	wincl.cbClsExtra = 0;					  /* No extra bytes after the window class */
+	wincl.cbWndExtra = 0;					  /* structure or the window instance */
+	/* Use Windows's default color as the background of the window */
+	wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+	
+	// Create the menu
+	HMENU hMenu = CreateMenu();
+	HMENU hMenuPopup = CreateMenu();
+	AppendMenu(hMenuPopup, MF_STRING, IDM_FILE_EXIT, TEXT("Exit"));
+	AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hMenuPopup, TEXT("File"));
 
-    /* Register the window class, and if it fails quit the program */
-    if (!RegisterClassEx (&wincl))
-        return 0;
+	/* Register the window class, and if it fails quit the program */
+	if (!RegisterClassEx (&wincl))
+		return 0;
 
-    /* The class is registered, let's create the program*/
+	/* The class is registered, let's create the program*/
 	// Create the window
-    hwnd = CreateWindowEx (
-           0,                   /* Extended possibilites for variation */
-           szClassName,         /* Classname */
-           "Jeff's App",       /* Title Text */
-           WS_OVERLAPPEDWINDOW, /* default window */
-           CW_USEDEFAULT,       /* Windows decides the position */
-           CW_USEDEFAULT,       /* where the window ends up on the screen */
-           544,                 /* The programs width */
-           375,                 /* and height in pixels */
-           HWND_DESKTOP,        /* The window is a child-window to desktop */
-           hMenu,                /* Menu */
-           hThisInstance,       /* Program Instance handler */
-           NULL                 /* No Window Creation data */
-           );
+	hwnd = CreateWindowEx (
+		   0,				   /* Extended possibilites for variation */
+		   szClassName,		 /* Classname */
+		   "Jeff's App",	   /* Title Text */
+		   WS_OVERLAPPEDWINDOW, /* default window */
+		   CW_USEDEFAULT,	   /* Windows decides the position */
+		   CW_USEDEFAULT,	   /* where the window ends up on the screen */
+		   544,				 /* The programs width */
+		   375,				 /* and height in pixels */
+		   HWND_DESKTOP,		/* The window is a child-window to desktop */
+		   hMenu,				/* Menu */
+		   hThisInstance,	   /* Program Instance handler */
+		   NULL				 /* No Window Creation data */
+		   );
 
 	// Create a button
 	HWND hwndButton = CreateWindow(
@@ -76,20 +76,20 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
 		(HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
 		NULL);				// Pointer not needed.
 
-    /* Make the window visible on the screen */
-    ShowWindow (hwnd, nFunsterStil);
+	/* Make the window visible on the screen */
+	ShowWindow (hwnd, nFunsterStil);
 
-    /* Run the message loop. It will run until GetMessage() returns 0 */
-    while (GetMessage (&messages, NULL, 0, 0))
-    {
-        /* Translate virtual-key messages into character messages */
-        TranslateMessage(&messages);
-        /* Send message to WindowProcedure */
-        DispatchMessage(&messages);
-    }
+	/* Run the message loop. It will run until GetMessage() returns 0 */
+	while (GetMessage (&messages, NULL, 0, 0))
+	{
+		/* Translate virtual-key messages into character messages */
+		TranslateMessage(&messages);
+		/* Send message to WindowProcedure */
+		DispatchMessage(&messages);
+	}
 
-    /* The program return-value is 0 - The value that PostQuitMessage() gave */
-    return messages.wParam;
+	/* The program return-value is 0 - The value that PostQuitMessage() gave */
+	return messages.wParam;
 }
 
 
@@ -97,25 +97,25 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
 
 LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)                  /* handle the messages */
-    {
-        case WM_COMMAND: // File Menu commands
-            switch(LOWORD(wParam))
-            {
-                case IDM_FILE_EXIT: // Exit command
-                     PostMessage(hwnd, WM_CLOSE, 0, 0); // Send a message to terminate the app
-                break;
+	switch (message)				  /* handle the messages */
+	{
+		case WM_COMMAND: // File Menu commands
+			switch(LOWORD(wParam))
+			{
+				case IDM_FILE_EXIT: // Exit command
+					 PostMessage(hwnd, WM_CLOSE, 0, 0); // Send a message to terminate the app
+				break;
 				case IDI_BUTTON: // Button Click
 					MessageBox(NULL, TEXT("You clicked the button!"), TEXT("Message"), MB_OK); // Create a popup
 				break;
-            }
-            break;
-        case WM_DESTROY:
-            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
-            break;
-        default:                      /* for messages that we don't deal with */
-            return DefWindowProc (hwnd, message, wParam, lParam);
-    }
+			}
+			break;
+		case WM_DESTROY:
+			PostQuitMessage (0);	   /* send a WM_QUIT to the message queue */
+			break;
+		default:					  /* for messages that we don't deal with */
+			return DefWindowProc (hwnd, message, wParam, lParam);
+	}
 
-    return 0;
+	return 0;
 }
